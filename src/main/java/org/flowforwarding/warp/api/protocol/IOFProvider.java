7c6790986b6a456c81bf2ca3c65079a7e88d7023
Dmitry Orekhov <dmitry_orekhov@epam.com>
2014-04-07 15:46:36 +0300

    intial commit

---
+++ src/main/java/org/flowforwarding/warp/api/protocol/IOFProvider.java
@@ -0,0 +1,15 @@
+/**
+ * © 2013 FlowForwarding.Org
+ * All Rights Reserved.  Use is subject to license terms.
+ */
+package org.flowforwarding.warp.api.protocol;
+
+/**
+ * @author Infoblox Inc.
+ *
+ */
+public interface IOFProvider {
+   
+   
+
+}
